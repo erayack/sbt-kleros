a27d25ccc80ce52377f5f4b56c7e1335f45e7ca5
Eray <erayack@Erays-Air.home>
2022-10-28 20:14:50 +0300

    improve SBT.sol

--- contracts/SBT.sol
+++ contracts/SBT.sol
@@ -1,40 +1,100 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
-
-
 /// Following the design of https://papers.ssrn.com/sol3/papers.cfm?abstract_id=4105763
 
-contract SBT is Ownable, ERC721URIStorage {
-    using SafeMath for uint256;
-    using Counters for Counters.Counter;
+contract SBT {
 
-    Counters.Counter private _tokenIds;
-    mapping(address => bool) public issuedSBT;
-    mapping(address => string) public accountToURI;
-
-    constructor(string memory _name, string memory _symbol)
-        ERC721(_name, _symbol)
-    {}
-
-    function issueSBT(address to) external onlyOwner {
-        require(!issuedSBT[to], "Token already issued");
-        issuedSBT[to] = true;
+    struct Soul {
+        string identity;
+        string url;
+        uint256 score;
+        uint256 timestamp;
     }
 
-    function claimSBT(string memory tokenURI) public returns (uint256) {
-        require(issuedSBT[_msgSender()], "Token not issued");
-        _tokenIds.increment();
-        uint256 newTokenId = _tokenIds.current();
-        _mint(_msgSender(), newTokenId);
+    mapping (address => Soul) private souls;
+    mapping (address => mapping (address => Soul)) soulProfiles;
+    mapping (address => address[]) private profiles;
 
-        issuedSBT[_msgSender()] = false;
-        accountToURI[_msgSender()] = tokenURI;
+    string public name;
+    string public ticker;
+    address public operator;
+    bytes32 private zeroHash = 0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470;
+    
+    event Mint(address _soul);
+    event Burn(address _soul);
+    event Update(address _soul);
+    event SetProfile(address _profiler, address _soul);
+    event RemoveProfile(address _profiler, address _soul);
 
-        return newTokenId;
+    constructor(string memory _name, string memory _ticker) {
+      name = _name;
+      ticker = _ticker;
+      operator = msg.sender;
+    }
+
+    function mint(address _soul, Soul memory _soulData) external {
+        require(keccak256(bytes(souls[_soul].identity)) == zeroHash, "Soul already exists");
+        require(msg.sender == operator, "Only operator can mint new souls");
+        souls[_soul] = _soulData;
+        emit Mint(_soul);
+    }
+
+    function burn(address _soul) external {
+        require(msg.sender == _soul || msg.sender == operator, "Only users and issuers have rights to delete their data");
+        delete souls[_soul];
+        for (uint i=0; i<profiles[_soul].length; i++) {
+            address profiler = profiles[_soul][i];
+            delete soulProfiles[profiler][_soul];
+        }
+        emit Burn(_soul);
+    }
+
+    function update(address _soul, Soul memory _soulData) external {
+        require(msg.sender == operator, "Only operator can update soul data");
+        require(keccak256(bytes(souls[_soul].identity)) != zeroHash, "Soul does not exist");
+        souls[_soul] = _soulData;
+        emit Update(_soul);
+    }
+
+    function hasSoul(address _soul) external view returns (bool) {
+        if (keccak256(bytes(souls[_soul].identity)) == zeroHash) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    function getSoul(address _soul) external view returns (Soul memory) {
+        return souls[_soul];
+    }
+
+    function setProfile(address _soul, Soul memory _soulData) external {
+        require(keccak256(bytes(souls[_soul].identity)) != zeroHash, "Cannot create a profile for a soul that has not been minted");
+        soulProfiles[msg.sender][_soul] = _soulData;
+        profiles[_soul].push(msg.sender);
+        emit SetProfile(msg.sender, _soul);
+    }
+
+    function getProfile(address _profiler, address _soul) external view returns (Soul memory) {
+        return soulProfiles[_profiler][_soul];
+    }
+
+    function listProfiles(address _soul) external view returns (address[] memory) {
+        return profiles[_soul];
+    }
+
+    function hasProfile(address _profiler, address _soul) external view returns (bool) {
+        if (keccak256(bytes(soulProfiles[_profiler][_soul].identity)) == zeroHash) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    function removeProfile(address _profiler, address _soul) external {
+        require(msg.sender == _soul, "Only users have rights to delete their profile data");
+        delete soulProfiles[_profiler][msg.sender];
+        emit RemoveProfile(_profiler, _soul);
     }
 }
